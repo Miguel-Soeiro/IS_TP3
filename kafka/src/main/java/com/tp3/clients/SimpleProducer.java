30e9d0faad4d313a65656ed70c04d4f9990054f7
Miguel-Soeiro <101994439+Miguel-Soeiro@users.noreply.github.com>
2022-11-21 16:38:29 +0000

    add gitignoreS

--- kafka/src/main/java/com/tp3/clients/SimpleProducer.java
+++ kafka/src/main/java/com/tp3/clients/SimpleProducer.java
@@ -1,5 +1,14 @@
 package com.tp3.clients;
 
+//import simple producer packages
+import org.apache.kafka.clients.producer.Producer;
+
+//import KafkaProducer packages
+import org.apache.kafka.clients.producer.KafkaProducer;
+
+//import ProducerRecord packages
+import org.apache.kafka.clients.producer.ProducerRecord;
+
 public class SimpleProducer {
     
 }
