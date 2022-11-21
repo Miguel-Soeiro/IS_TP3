08edb36756caed6a3cc8c4584521aab123b5fbff
Miguel-Soeiro <101994439+Miguel-Soeiro@users.noreply.github.com>
2022-11-21 16:36:35 +0000

    update

---
+++ kafka/src/main/java/com/tp3/clients/SimpleConsumer.java
@@ -0,0 +1,9 @@
+package com.tp3.clients;
+
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+
+public class SimpleConsumer {
+    
+}
---
+++ kafka/src/main/java/com/tp3/clients/SimpleProducer.java
@@ -0,0 +1,5 @@
+package com.tp3.clients;
+
+public class SimpleProducer {
+    
+}
